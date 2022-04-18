14ea911db76eeeb1a7f2d15f6f61d9f4ef0ffa40
zff85277708 <602192932@qq.com>
2022-04-18 15:37:36 +0800

    factory

---
+++ src/main/java/com/designPattern/found/factory/method/IRuleConfigParser.java
@@ -0,0 +1,5 @@
+package com.designPattern.found.factory.method;
+
+public interface IRuleConfigParser {
+    RuleConfig parse(String configText);
+}
---
+++ src/main/java/com/designPattern/found/factory/method/InvalidRuleConfigException.java
@@ -0,0 +1,7 @@
+package com.designPattern.found.factory.method;
+
+public class InvalidRuleConfigException extends RuntimeException {
+    public InvalidRuleConfigException(String message) {
+        super(message);
+    }
+}
