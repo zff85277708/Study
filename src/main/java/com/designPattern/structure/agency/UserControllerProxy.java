19ee42305e108795194fadef918017ac044c0856
zff85277708 <602192932@qq.com>
2022-04-23 10:00:30 +0800

    代理模式

--- src/main/java/com/designPattern/structure/agency/UserControllerProxy.java
+++ src/main/java/com/designPattern/structure/agency/UserControllerProxy.java
@@ -16,12 +16,31 @@ public class UserControllerProxy extends UserController {
 
         UserVo userVo = super.login(telephone, password);
 
-
         long endTimeStamp = System.currentTimeMillis();
         long responseTime = endTimeStamp - startTimestamp;
         RequestInfo requestInfo = new RequestInfo("login", responseTime, startTimestamp);
         metricsCollector.recordRequest(requestInfo);
 
         return userVo;
+    }
+
+    @Override
+    public UserVo register(String telephone, String password) {
+        long startTimestamp = System.currentTimeMillis();
+
+        UserVo userVo = super.register(telephone, password);
+
+        long endTimeStamp = System.currentTimeMillis();
+        long responseTime = endTimeStamp - startTimestamp;
+        RequestInfo requestInfo = new RequestInfo("register", responseTime, startTimestamp);
+        metricsCollector.recordRequest(requestInfo);
+
+        return userVo;
+    }
+
+    public static void main(String[] args) {
+        IUserController userController = new UserControllerProxy();
+        UserVo user = userController.login("13916263601", "88888");
+        System.out.println(user);
     }
 }
--- src/main/java/com/designPattern/structure/agency/UserVo.java
+++ src/main/java/com/designPattern/structure/agency/UserVo.java
@@ -9,19 +9,8 @@ public class UserVo {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
+    @Override
+    public String toString() {
+        return "name: " + name + ",age: " + age;
     }
 }
