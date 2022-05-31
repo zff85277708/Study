99edb06ffbc47307a73ac878840c579db2a69779
zff85277708 <602192932@qq.com>
2022-05-31 13:54:09 +0800

    leetcode

--- src/main/java/com/leetcode/easy/IsSymmetric.java
+++ src/main/java/com/leetcode/easy/IsSymmetric.java
@@ -9,7 +9,7 @@ public class IsSymmetric {
 
     private boolean check(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
-        if (p  == null || q == null) return false;
+        if (p == null || q == null) return false;
         return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
     }
 }
