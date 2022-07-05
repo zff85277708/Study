71a44aa35abeba149788599f04fa7db195fd25b2
zff85277708 <602192932@qq.com>
2022-07-05 12:28:54 +0800

    algorithm

--- src/main/java/com/algorithm/LinkedLRU.java
+++ src/main/java/com/algorithm/LinkedLRU.java
@@ -4,6 +4,10 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.UUID;
 
+/**
+ * 链表实现LRU缓存
+ * @param <E>
+ */
 public class LinkedLRU<E> {
     private Node<E> first;
     private Node<E> last;
@@ -19,28 +23,46 @@ public class LinkedLRU<E> {
         this.limit = limit;
     }
 
-    public E search(String id) {
-        Node<E> next = first;
-        Node<E> pre = null;
-        while (next != null) {
-            if (id.equals(next.id)) {
+    public synchronized E search(String id) {
+        if (0 == size) return null;
+        if(id.equals(first.id)) return first.item;
 
-                return next.item;
+        Node<E> next = first;
+        while (next.next != null) {
+            if (id.equals(next.next.id)) {
+                Node<E> tmp = next.next;
+                next.next = next.next.next;
+                //尾节点命中，重新更新尾节点
+                if (next.next == null) last = next;
+                tmp.next = first;
+                first = tmp;
+                return tmp.item;
             }
+            next = next.next;
         }
         return null;
+    }
+
+    public synchronized void add(String id, E item) {
+        if (0 == size) {
+            first = new Node<>(item, null, id);
+            last = first;
+        } else if (size >= limit) {
+
+        } else {
+            last.next = new Node<>(item, null, id);
+            last = last.next;
+        }
     }
 
     private static class Node<E> {
         E item;
         Node<E> next;
-        long timestamp;
         String id;
 
-        Node(E item, Node<E> next, long timestamp, String id) {
+        Node(E item, Node<E> next, String id) {
             this.item = item;
             this.next = next;
-            this.timestamp = timestamp;
             this.id = id;
         }
     }
