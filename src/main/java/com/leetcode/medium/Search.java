91b9ad782a4f15a21744da07a8206c46f6ded8f6
zff85277708 <602192932@qq.com>
2022-07-15 10:52:32 +0800

    leetcode

--- src/main/java/com/leetcode/medium/Search.java
+++ src/main/java/com/leetcode/medium/Search.java
@@ -2,12 +2,27 @@ package com.leetcode.medium;
 
 public class Search {
     public int search(int[] nums, int target) {
-        if (nums.length <= 0) return -1;
+        int n = nums.length;
+        if (n == 0) return -1;
+        if (n == 1) return nums[0] == target ? 0 : -1;
 
-        int low = 0;
-        int high = nums.length - 1;
-        while (low < high) {
-            int mid = 0;
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) return mid;
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            else {
+                if (target >= nums[mid + 1] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
         }
 
         return -1;
